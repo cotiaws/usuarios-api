b712d4be9bc7fa1a0d1887ee49938032928db53e
sergiosistemapeoplenet <sergio@sistemapeoplenet.com.br>
2025-08-12 08:53:55 -0300

    Desenvolvimento dos testes unitários e de integração.

--- src/main/java/br/com/cotiinformatica/entities/Usuario.java
+++ src/main/java/br/com/cotiinformatica/entities/Usuario.java
@@ -7,8 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Usuario {
 
 	@Id
@@ -23,37 +25,4 @@ public class Usuario {
 
 	@Column(length = 100, nullable = false)
 	private String senha;
-
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
 }
